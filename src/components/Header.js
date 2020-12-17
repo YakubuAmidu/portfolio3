df333cea959909d72de6499a93dce0f58bbb4cff
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-17 09:25:48 -0800

    Initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,24 +6,20 @@ class Header extends Component {
     return (
       <div>
         <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
-          <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href='#home'>
+            <i className='fas fa-user-tie'></i>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls='responsive-navbar-nav' />
           <Navbar.Collapse id='responsive-navbar-nav'>
             <Nav className='mr-auto'>
-              <Nav.Link href='#features'>Features</Nav.Link>
-              <Nav.Link href='#pricing'>Pricing</Nav.Link>
-              <NavDropdown title='Dropdown' id='collasible-nav-dropdown'>
-                <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-                <NavDropdown.Item href='#action/3.2'>
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href='#action/3.3'>
-                  Something
-                </NavDropdown.Item>
+              <Nav.Link href='#features'>PORTFOLIO</Nav.Link>
+              <Nav.Link href='#pricing'>ABOUT</Nav.Link>
+              <Nav.Link href='#pricing'>CONTACT</Nav.Link>
+              <NavDropdown title='DROPDOWN' id='collasible-nav-dropdown'>
+                <NavDropdown.Item href='#'>PORTFOLIO</NavDropdown.Item>
+                <NavDropdown.Item href='#'>ABOUT</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href='#action/3.4'>
-                  Separated link
-                </NavDropdown.Item>
+                <NavDropdown.Item href='#'>CONTACT</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
