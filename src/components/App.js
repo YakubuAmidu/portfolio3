4e562a281d8a1f297ee647fbc3bca99a85f02a9a
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-17 08:49:14 -0800

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,20 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1 className='heading'>SOFWARE DEVELOPER</h1>
-        <h3 className='skills'>
-          CSS, GRID, FLEX-BOX, ANIMATION, BOOTSTRAP, SASS
-        </h3>
-        <div className='findoutmore'>Find out more</div>
-        <div className='social-links'>
-          <p className='connect'>Connect with me</p>
-          <i className='fab fa-github-square'></i>
-          <i className='fab fa-linkedin'></i>
-          <i className='fab fa-twitter-square'></i>
-          <i className='fab fa-facebook-square'></i>
-          <i className='fab fa-facebook-square'></i>
+        <div className='app-header'>
+          <h1 className='heading'>SOFWARE DEVELOPER</h1>
+          <h3 className='skills'>
+            CSS, GRID, FLEX-BOX, ANIMATION, BOOTSTRAP, SASS
+          </h3>
+          <div className='findoutmore'>Find out more</div>
+          <div className='social-links'>
+            <p className='connect'>Connect with me</p>
+            <i className='fab fa-github-square'></i>
+            <i className='fab fa-linkedin'></i>
+            <i className='fab fa-twitter-square'></i>
+            <i className='fab fa-facebook-square'></i>
+            <i className='fab fa-facebook-square'></i>
+          </div>
         </div>
       </div>
     );
