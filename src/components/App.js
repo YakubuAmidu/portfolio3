db2a52f58c4d1b0706b77bac8f3328c9c5ff5b0b
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-17 09:08:29 -0800

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from './Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <Header />
         <div className='app-header'>
           <h1 className='heading'>SOFWARE DEVELOPER</h1>
           <h3 className='skills'>
