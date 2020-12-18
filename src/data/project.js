dac720efb11e671e2eb821a232af8aa871cd11db
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-18 10:41:34 -0800

    Commiting changes

---
+++ src/data/project.js
@@ -0,0 +1,34 @@
+import github from '../assets/github.png';
+import linkedin from '../assets/linkedin.png';
+import twitter from '../assets/twitter.png';
+import facebook from '../assets/facebook.png';
+import instagram from '../assets/instagram.png';
+
+const PROJECTS = [
+  {
+    id: 1,
+    link:
+      'https://github.com/YakubuAmidu/portfolio/blob/master/src/data/socialProfiles.js',
+    image: github,
+  },
+  {
+    id: 2,
+    link:
+      'https://github.com/YakubuAmidu/portfolio/blob/master/src/data/socialProfiles.js',
+    image: twitter,
+  },
+  {
+    id: 3,
+    link:
+      'https://github.com/YakubuAmidu/portfolio/blob/master/src/data/socialProfiles.js',
+    image: facebook,
+  },
+  {
+    id: 4,
+    link:
+      'https://github.com/YakubuAmidu/portfolio/blob/master/src/data/socialProfiles.js',
+    image: instagram,
+  },
+];
+
+export default PROJECTS;
