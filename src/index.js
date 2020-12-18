fdd4648925440d1a0e3d477def09f3d8222e5fa0
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-18 01:33:36 -0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+import Portfolio from './components/Portfolio';
+import { Router, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router history={createBrowserHistory()}>
+    <Switch>
+      <Route exact path='/' component={App} />
+      <Route path='/portfolio' component={Portfolio} />
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
