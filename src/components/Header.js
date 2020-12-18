133b0ff9dfbb395203b5fa7421d3f7850dca67cd
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-18 05:11:46 -0800

    Initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
       <div>
         <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
           <Navbar.Brand href='#home'>
-            <i className='fas fa-user-tie'></i>
+            <i class='fas fa-laptop-code'></i>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='responsive-navbar-nav' />
           <Navbar.Collapse id='responsive-navbar-nav'>
