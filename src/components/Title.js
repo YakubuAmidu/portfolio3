56fbdfa4ac63900286ae040a9e8c581e1db0bbf6
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-19 04:07:31 -0800

    Initial commit

--- src/components/Title.js
+++ src/components/Title.js
@@ -8,8 +8,37 @@ const TITLES = [
 ];
 
 class Title extends Component {
+  state = { titleIndex: 0, fadeIn: true };
+
+  componentDidMount() {
+    this.timeout = setTimeout(() => this.setState({ fadeIn: false }), 2000);
+
+    this.animateTitles();
+  }
+
+  componentWillMount() {
+    clearInterval(this.titleInterval);
+    clearTimeout(this.timeout);
+  }
+
+  animateTitles = () => {
+    this.titleInterval = setInterval(() => {
+      const titleIndex = (this.state.titleIndex + 1) % TITLES.length;
+
+      this.setState({ titleIndex, fadeIn: true });
+
+      this.timeout = setTimeout(() => this.setState({ fadeIn: false }), 2000);
+    }, 2000);
+  };
+
   render() {
-    return <div></div>;
+    const { fadeIn, titleIndex } = this.state;
+
+    const title = TITLES[titleIndex];
+
+    return (
+      <p className={fadeIn ? 'title-fade-in' : 'title-fade-out'}>{title}</p>
+    );
   }
 }
 
